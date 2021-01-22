ed0488176d008ffd98ec5e42cdc525cff8dc8aec
like <13521819338@163.com>
2021-01-22 17:26:12 +0800

    动态数组自定义接口初步实现

---
+++ mall-data-structure/src/main/java/com/huatech/mall/array/ArrayList0.java
@@ -0,0 +1,216 @@
+package com.huatech.mall.array;
+
+/**
+ * @author like
+ * @date 2021-01-22 2:54 下午
+ **/
+public class ArrayList0<E> {
+
+
+    /**
+     * 元素的数量
+     */
+    private int size;
+
+    /**
+     * 所有的元素
+     */
+    private E[] elements;
+
+    /**
+     * 默认长度
+     */
+    private static final int DEFAULT_CAPACITY = 10;
+
+
+    public ArrayList0(int capacity) {
+        capacity = (capacity < DEFAULT_CAPACITY) ? DEFAULT_CAPACITY : capacity;
+        elements = (E[]) new Object[capacity];
+    }
+
+    public ArrayList0() {
+        this(DEFAULT_CAPACITY);
+    }
+
+    /**
+     * 清除所有元素
+     */
+    public void clear() {
+        for (int i = 0; i < size; i++) {
+            elements[i] = null;
+        }
+        size = 0;
+    }
+
+    /**
+     * 元素的数量
+     *
+     * @return
+     */
+    public int size() {
+        return size;
+    }
+
+    /**
+     * 是否为空
+     *
+     * @return
+     */
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    /**
+     * 是否包含某个元素
+     *
+     * @param element
+     * @return
+     */
+    public boolean contains(E element) {
+        return indexOf(element) == -1;
+    }
+
+    /**
+     * 添加元素到尾部
+     *
+     * @param element
+     */
+    public void add(E element) {
+        add(size, element);
+    }
+
+    /**
+     * 获取index位置的元素
+     *
+     * @param index
+     * @return
+     */
+    public E get(int index) {
+        rangeCheck(index);
+        return elements[index];
+    }
+
+    /**
+     * 设置index位置的元素
+     *
+     * @param index
+     * @param element
+     * @return 原来的元素ֵ
+     */
+    public E set(int index, E element) {
+        rangeCheck(index);
+        E old = elements[index];
+        elements[index] = element;
+        return old;
+    }
+
+    /**
+     * 在index位置插入一个元素
+     * 1 2 3 4 5
+     *
+     * @param index
+     * @param element
+     */
+    public void add(int index, E element) {
+
+        rangeCheckForAdd(index);
+        //size和elements的length是不一样的
+        ensureCapacity(size + 1);
+        for (int i = size; index < i; i--) {
+            elements[i] = elements[i - 1];
+        }
+        elements[index] = element;
+        size++;
+    }
+
+    /**
+     * 删除index位置的元素
+     *
+     * @param index
+     * @return
+     */
+    public E remove(int index) {
+        rangeCheck(index);
+        E old = elements[index];
+        // 1 2 3 4
+        for (int i = index; i <= size; i++) {
+            elements[i] = elements[i + 1];
+        }
+        size--;
+        return old;
+    }
+
+    /**
+     * 查看元素的索引
+     *
+     * @param element
+     * @return
+     */
+    public int indexOf(E element) {
+        for (int i = 0; i < size; i++) {
+            if (elements[i].equals(element)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+
+    /**
+     * 保证要有capacity的容量
+     * 扩容
+     *
+     * @param capacity
+     */
+    private void ensureCapacity(int capacity) {
+        int oldCapacity = elements.length;
+        if (oldCapacity >= capacity) {
+            return;
+        }
+        //右移1除以2
+        //左移1是乘以2
+        int newCapacity = oldCapacity + oldCapacity >> 1;
+        E[] newElements = (E[]) new Object[newCapacity];
+        for (int i = 0; i < size; i++) {
+            newElements[i] = elements[i];
+        }
+        //elements指向新的数组
+        elements = newElements;
+    }
+
+
+    private void rangeCheckForAdd(int index) {
+
+        if (index < 0 || index > size) {
+            throw new IndexOutOfBoundsException("Index: " + index + ", Size: " + size);
+        }
+    }
+
+    @Override
+    public String toString() {
+
+        StringBuilder builder = new StringBuilder();
+        builder.append("size=").append(size).append(",[");
+        for (int i = 0; i < size; i++) {
+            builder.append(elements[i]);
+            if (i != size - 1) {
+                builder.append(",");
+            }
+        }
+
+        builder.append("]");
+        return builder.toString();
+    }
+
+
+    /**
+     * 校验范围
+     *
+     * @param index
+     */
+    private void rangeCheck(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException("Index: " + index + ", Size: " + size);
+        }
+    }
+}
