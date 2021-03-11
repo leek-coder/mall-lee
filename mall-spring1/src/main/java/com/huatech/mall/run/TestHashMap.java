ff376a92efd47494530499ad1ed8333a708e2d4f
like <13521819338@163.com>
2021-03-11 17:38:33 +0800

    [UPD]更新数据

---
+++ mall-spring1/src/main/java/com/huatech/mall/run/TestHashMap.java
@@ -0,0 +1,19 @@
+package com.huatech.mall.run;
+
+import java.util.HashMap;
+
+/**
+ * @author like
+ * @date 2021-03-03 5:24 下午
+ **/
+public class TestHashMap {
+    public static void main(String[] args) {
+
+        HashMap<String, String> hashMap = new HashMap<>();
+        hashMap.put("name", "李苛");
+        System.out.println(hashMap);
+//        每天都需要不断的去学习，这样才会不断的提高，即使每天只进步一点点，日积月累也会有很大的收获
+
+
+    }
+}
